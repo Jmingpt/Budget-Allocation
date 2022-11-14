6d2c0658b4f130cd0413e612777f9d066dc488fa
Jmingpt <88297237+Jmingpt@users.noreply.github.com>
2022-11-14 11:15:31 +0800

    Delete sideBar.py

--- module/mmm/sideBar.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def sidebarTool():
-    with st.sidebar:
-        tool_options = st.radio('Select the tool:', ['Spend Ops', 'MMM Model', 'MTA Model', 'Markov Model'])
-
-    return tool_options
